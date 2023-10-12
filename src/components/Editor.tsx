d90727ee0d950487367d530e19677d79012ae14f
Yug34 <vertilax1337@gmail.com>
2023-10-12 17:58:25 +0530

    display first frame of video

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,9 +1,9 @@
-import {fetchFile, toBlobURL} from '@ffmpeg/util';
+import { fetchFile, toBlobURL } from '@ffmpeg/util';
 import { ChangeEvent, useEffect, useRef, useState } from "react";
 import { FileData } from "@ffmpeg/ffmpeg/dist/esm/types";
-import {StyledButton} from "../App";
+import { StyledButton } from "../App";
 import styled from "styled-components";
-import {FFmpeg} from "@ffmpeg/ffmpeg";
+import { FFmpeg } from "@ffmpeg/ffmpeg";
 
 const StyledLabel = styled.label`
   color: white;
@@ -22,7 +22,9 @@ const Editor = () => {
     const messageRef = useRef<HTMLParagraphElement | null>(null);
     const fileInputRef = useRef<HTMLInputElement | null>(null);
 
+    const [isLoaded, setIsLoaded] = useState<boolean>(false);
     const [video, setVideo] = useState<Uint8Array | null>(null);
+    const [videoThumbnail, setVideoThumbnail] = useState<string | null>(null);
     const ffmpegRef = useRef(new FFmpeg());
 
     const load = async () => {
@@ -35,6 +37,8 @@ const Editor = () => {
             coreURL: await toBlobURL(`${baseURL}/ffmpeg-core.js`, 'text/javascript'),
             wasmURL: await toBlobURL(`${baseURL}/ffmpeg-core.wasm`, 'application/wasm')
         });
+
+        setIsLoaded(true);
     }
 
     useEffect(() => {
@@ -44,37 +48,65 @@ const Editor = () => {
         });
     }, []);
 
+    useEffect(() => {
+        (async () => {
+            if (video) {
+                const ffmpeg = ffmpegRef.current;
+                await ffmpeg.writeFile('input.webm', video!);
+                // ffmpeg -i inputfile.mkv -vf "select=eq(n\,0)" -q:v 3 output_image.jpg
+                await ffmpeg.exec(['-i', 'input.webm', '-vf', 'select=eq(n\\,0)', "-q:v", "3", "output_image.png"]);
+                const data: FileData = await ffmpeg.readFile('output_image.png');
+                setVideoThumbnail(URL.createObjectURL(
+                    new Blob([data as Uint8Array], { type: 'image/png' })
+                ));
+                await ffmpeg.deleteFile('output_image.png')
+            }
+        })();
+
+        return () => {
+        };
+    }, [video]);
+
+    useEffect(() => {
+        console.log(videoThumbnail)
+    }, [videoThumbnail]);
+
     const transcode = async () => {
         const ffmpeg = ffmpegRef.current;
-        await ffmpeg.writeFile('input.webm', video!);
         await ffmpeg.exec(['-i', 'input.webm', 'output.mp4']);
         const data: FileData = await ffmpeg.readFile('output.mp4');
         videoRef.current!.src = URL.createObjectURL(new Blob([data], {type: 'video/mp4'}));
     }
 
     return (
-        <div>
-            <>
-                {video ? (
-                    <>
-                        <video ref={videoRef} controls />
-                        <StyledButton onClick={transcode}>Transcode webm to mp4</StyledButton>
-                        <p ref={messageRef}></p>
-                    </>
-                ) : (
-                    <>
-                        <StyledLabel htmlFor="file-upload" className="custom-file-upload">Custom Upload</StyledLabel>
-                        <input
-                            style={{display: "none"}} id="file-upload" type="file" ref={fileInputRef}
-                            onChange={async (e: ChangeEvent) => {
-                                console.log("Uploaded video");
-                                setVideo(await fetchFile((e.target as HTMLInputElement)!.files![0]));
-                            }}
-                            onClick={load}
-                        />
-                    </>
-                )}
-            </>
+        <div
+            style={{
+                display: "flex",
+                flexDirection: "column",
+                rowGap: "1rem"
+            }}
+        >
+            {video && isLoaded ? (
+                <>
+                    {videoThumbnail && <img src={videoThumbnail} alt={"thumbnail"} />}
+                    <video ref={videoRef} controls />
+                    <StyledButton onClick={transcode}>Transcode webm to mp4</StyledButton>
+                    <p style={{width: "400px"}} ref={messageRef}></p>
+                </>
+            ) : (
+                <>
+                    <StyledLabel htmlFor="file-upload" className="custom-file-upload">{video ? "Loading ffmpeg" : "Upload video"}</StyledLabel>
+                    <input
+                        style={{display: "none"}} id="file-upload" type="file" ref={fileInputRef}
+                        onClick={video ? () => {} : load}
+                        onChange={async (e: ChangeEvent) => {
+                            console.log("Uploaded video");
+                            const file = (e.target as HTMLInputElement)!.files![0];
+                            setVideo(await fetchFile(file));
+                        }}
+                    />
+                </>
+            )}
         </div>
     );
 }
