d36b244dc29164b0972fca5265081c0bdf06a3e3
Yug34 <vertilax1337@gmail.com>
2023-10-12 16:06:30 +0530

    single click UX

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,6 @@
 // so basically steps through different transformations and applies all the effects sequentially
 // i think just load ffmpeg when they click on upload video
 
-import { FFmpeg } from '@ffmpeg/ffmpeg';
-import { toBlobURL } from '@ffmpeg/util';
-import { useRef, useState } from "react";
 import Editor from "./components/Editor";
 import styled from "styled-components";
 
@@ -34,32 +31,9 @@ export const StyledButton = styled.button`
 `;
 
 const App = () => {
-    const [loaded, setLoaded] = useState<boolean>(false);
-    const ffmpegRef = useRef(new FFmpeg());
-
-    const load = async () => {
-        const baseURL = 'https://unpkg.com/@ffmpeg/core@0.12.4/dist/umd';
-        const ffmpeg = ffmpegRef.current;
-
-        // toBlobURL is used to bypass CORS issue, urls with the same
-        // domain can be used directly.
-        await ffmpeg.load({
-            coreURL: await toBlobURL(`${baseURL}/ffmpeg-core.js`, 'text/javascript'),
-            wasmURL: await toBlobURL(`${baseURL}/ffmpeg-core.wasm`, 'application/wasm')
-        });
-
-        setLoaded(true);
-    }
-
     return (
       <Layout>
-        {loaded ? (
-            <Editor ffmpeg={ffmpegRef.current}/>
-        ) : (
-            <StyledButton onClick={load}>
-                Load ffmpeg
-            </StyledButton>
-        )}
+          <Editor />
       </Layout>
   );
 }
--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,8 +1,9 @@
-import { fetchFile } from '@ffmpeg/util';
+import {fetchFile, toBlobURL} from '@ffmpeg/util';
 import { ChangeEvent, useEffect, useRef, useState } from "react";
 import { FileData } from "@ffmpeg/ffmpeg/dist/esm/types";
 import {StyledButton} from "../App";
 import styled from "styled-components";
+import {FFmpeg} from "@ffmpeg/ffmpeg";
 
 const StyledLabel = styled.label`
   color: white;
@@ -16,26 +17,35 @@ const StyledLabel = styled.label`
   }
 `;
 
-type EditorProps = {
-    ffmpeg: any;
-};
-
-const Editor = ({ffmpeg}: EditorProps) => {
+const Editor = () => {
     const videoRef = useRef<HTMLVideoElement | null>(null);
     const messageRef = useRef<HTMLParagraphElement | null>(null);
     const fileInputRef = useRef<HTMLInputElement | null>(null);
 
     const [video, setVideo] = useState<Uint8Array | null>(null);
+    const ffmpegRef = useRef(new FFmpeg());
+
+    const load = async () => {
+        const baseURL = 'https://unpkg.com/@ffmpeg/core@0.12.4/dist/umd';
+        const ffmpeg = ffmpegRef.current;
+
+        // toBlobURL is used to bypass CORS issue, urls with the same
+        // domain can be used directly.
+        await ffmpeg.load({
+            coreURL: await toBlobURL(`${baseURL}/ffmpeg-core.js`, 'text/javascript'),
+            wasmURL: await toBlobURL(`${baseURL}/ffmpeg-core.wasm`, 'application/wasm')
+        });
+    }
 
     useEffect(() => {
-        // @ts-ignore
-        ffmpeg.on('log', ({ message }) => {
+        ffmpegRef.current.on('log', ({ message }) => {
             messageRef.current!.innerHTML = message;
             console.log(message);
         });
     }, []);
 
     const transcode = async () => {
+        const ffmpeg = ffmpegRef.current;
         await ffmpeg.writeFile('input.webm', video!);
         await ffmpeg.exec(['-i', 'input.webm', 'output.mp4']);
         const data: FileData = await ffmpeg.readFile('output.mp4');
@@ -44,21 +54,27 @@ const Editor = ({ffmpeg}: EditorProps) => {
 
     return (
         <div>
-            {video ? (
-                <>
-                    <video ref={videoRef} controls />
-                    <StyledButton onClick={transcode}>Transcode webm to mp4</StyledButton>
-                    <p ref={messageRef}></p>
-                </>
-            ) : (
-                <>
-                    <StyledLabel htmlFor="file-upload" className="custom-file-upload">Custom Upload</StyledLabel>
-                    <input style={{display: "none"}} id="file-upload" type="file" ref={fileInputRef} onChange={async (e: ChangeEvent) => {
-                        console.log("Uploaded video");
-                        setVideo(await fetchFile((e.target as HTMLInputElement)!.files![0]));
-                    }}/>
-                </>
-            )}
+            <>
+                {video ? (
+                    <>
+                        <video ref={videoRef} controls />
+                        <StyledButton onClick={transcode}>Transcode webm to mp4</StyledButton>
+                        <p ref={messageRef}></p>
+                    </>
+                ) : (
+                    <>
+                        <StyledLabel htmlFor="file-upload" className="custom-file-upload">Custom Upload</StyledLabel>
+                        <input
+                            style={{display: "none"}} id="file-upload" type="file" ref={fileInputRef}
+                            onChange={async (e: ChangeEvent) => {
+                                console.log("Uploaded video");
+                                setVideo(await fetchFile((e.target as HTMLInputElement)!.files![0]));
+                            }}
+                            onClick={load}
+                        />
+                    </>
+                )}
+            </>
         </div>
     );
 }
